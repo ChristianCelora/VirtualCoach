21d3b6a2b9c463e91b4a1b5d2c9281827e4d58cb
christian.celora@gmail.com <christian.celora@gmail.com>
2020-03-18 09:59:38 +0100

    changes for define user role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,37 +4,47 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-class LoginController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+class LoginController extends Controller {
+   /*
+   |--------------------------------------------------------------------------
+   | Login Controller
+   |--------------------------------------------------------------------------
+   |
+   | This controller handles authenticating users for the application and
+   | redirecting them to your home screen. The controller uses a trait
+   | to conveniently provide its functionality to your applications.
+   |
+   */
 
-    use AuthenticatesUsers;
+   use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+   /**
+   * Where to redirect users after login.
+   *
+   * @var string
+   */
+   protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+   /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+   public function __construct(){
+      $this->middleware("guest")->except("logout");
+   }
+
+   // public function login(Request $request)[
+   //    $user = User::select("name")
+   //       ->whereColumn("email", $request->email)
+   //       ->whereColumn("password", $request->email)
+   //       ->get();
+   // }
+
+   // public function logout(){
+   //    return redirect("/login");
+   // }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -38,7 +38,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware("guest");
     }
 
     /**
@@ -50,9 +50,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            "name" => ["required", "string", "max:255"],
+            "email" => ["required", "string", "email", "max:255", "unique:users"],
+            "password" => ["required", "string", "min:8", "confirmed"],
+            "role" => ["required", "string", "email", "max:255"],
         ]);
     }
 
@@ -65,9 +66,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "password" => Hash::make($data["password"]),
+            "role" => (isset($data["role"])) ? $data["role"] : "client",
         ]);
     }
 }
