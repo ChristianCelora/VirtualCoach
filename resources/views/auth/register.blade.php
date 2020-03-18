9a6d87757366a931d08af48bbddeda93022e3cc4
christian.celora@gmail.com <christian.celora@gmail.com>
2020-03-18 09:59:38 +0100

    changes for define user role

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,14 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">Are you a trainer?</label>
+
+                            <div class="col-md-6">
+                                <input id="role" type="checkbox" class="form-control" name="role" value="trainer">
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,20 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+// Route::get('login', ['as' => 'auth.login', 'uses' => 'Auth\LoginController@showLoginForm']);
+// Route::get('post', ['as' => 'auth.login', 'uses' => 'Auth\LoginController@login']);
+// Route::get('logout', ['as' => 'auth.logout', 'uses' => 'Auth\LoginController@logout']);
+//
+// // Registration Routes...
+// Route::get('register', ['as' => 'auth.register', 'uses' => 'Auth\RegisterController@showRegistrationForm']);
+// Route::post('register', ['as' => 'auth.register', 'uses' => 'Auth\RegisterController@register']);
+//
+// // Password Reset Routes...
+// Route::get('password/reset/{token?}', ['as' => 'auth.password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
+// Route::get('password/email', ['as' => 'auth.password.email',  'uses' => 'Auth\ResetPasswordController@sendResetLinkEmail']);
+// Route::post('password/reset', ['as' => 'auth.password.reset', 'uses' => 'Auth\ResetPasswordController@reset']);
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/test', function () {
+    return view('welcome');
+});
