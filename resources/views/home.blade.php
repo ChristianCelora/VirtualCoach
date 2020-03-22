f902e37b7819667011346adb01c64dfb261fcba5
christian.celora@gmail.com <christian.celora@gmail.com>
2020-03-22 15:30:48 +0100

    Styled homepage links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,25 +14,24 @@
                         </div>
                     @endif
                        <div class="col-12 h-100 d-flex flex-row align-items-center">
+
                           {{-- User --}}
-                          <div class="col-4 p-0 row justify-content-center home-card">
-                              <div class="col-md-12">
-                                  <div class="card transparent-card">
-                                     <div class="card-body d-flex justify-content-center">
-                                        <div class="font-primary d-flex flex-column">
-                                           <div class="m-auto"><h3 class="font-weight-bold">User</h3></div>
-                                           <div class="m-auto"><i class="fas fa-user fa-2x"></i></div>
-                                        </div>
+                          <div class="col-4 p-0 row justify-content-center home-card font-primary">
+                               <div class="card transparent-card">
+                                  <div class="card-body d-flex justify-content-center">
+                                     <div class="d-flex flex-column">
+                                        <div class="m-auto"><h3 class="font-weight-bold">User</h3></div>
+                                        <div class="m-auto"><i class="fas fa-user fa-2x"></i></div>
                                      </div>
                                   </div>
                                </div>
                            </div>
 
                            {{-- Manage Trainings --}}
-                          <div class="col-4 p-0 row justify-content-center home-card">
-                               <div class="card transparent-card">
-                                  <div class="card-body d-flex justify-content-center">
-                                     <div class="font-primary d-flex flex-column">
+                           <div class="col-4 p-0 row justify-content-center home-card font-primary">
+                                <div class="card transparent-card">
+                                   <div class="card-body d-flex justify-content-center">
+                                      <div class="d-flex flex-column">
                                         <div class="m-auto"><h3 class="font-weight-bold">Manage Trainings</h3></div>
                                         <div class="m-auto"><i class="fas fa-clipboard-list fa-2x"></i></div>
                                      </div>
@@ -41,10 +40,10 @@
                            </div>
 
                            {{-- Work out --}}
-                          <div class="col-4 p-0 row justify-content-center home-card">
+                          <div class="col-4 p-0 row justify-content-center home-card font-primary">
                                <div class="card transparent-card">
                                   <div class="card-body d-flex justify-content-center">
-                                     <div class="font-primary d-flex flex-column">
+                                     <div class="d-flex flex-column">
                                         <div class="m-auto"><h3 class="font-weight-bold">Workout!</h3></div>
                                         <div class="m-auto"><i class="fas fa-dumbbell fa-2x"></i></div>
                                      </div>
