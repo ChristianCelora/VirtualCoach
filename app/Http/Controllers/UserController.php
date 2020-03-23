c3069b3f06aa6636f43603d679e2ebc2d17d9d3a
christian.celora@gmail.com <christian.celora@gmail.com>
2020-03-23 16:23:36 +0100

    Created container card

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller {
 
    public function showInfo(){
-
+      return view("user_info");
    }
 
    public function showTrainings(){
---
+++ resources/views/user_info.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container h-100">
+   <div class="mt-4 d-flex justify-content-center align-items-stretch">
+      <div class="card transparent-card user-card">
+
+         <div class="card-header d-flex align-items-start"><h3 class="font-weight-bold">User Info</h3></div>
+
+         <div class="card-body p-0">
+            <div class="col-md-3 col-sm-2 user-menu p-0">
+               <ul class="py-2 px-1 mb-0">
+                  <li><h4 >User info</h4></li>
+                  <li><h4 >Physical info</h4></li>
+                  <li><h4 >---Coming soon---</h4></li>
+               </ul>
+            </div>
+            <div class="col-md-10">
+
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+@endsection
